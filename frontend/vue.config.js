3c9d8c5b43cfbb12e97514466e2ed93280d2095f
John Dave Lagdameo <jdlagdameo@pldtglobal.com>
2021-01-24 19:25:02 +0800

    finalized the project

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    // publicPath: path.resolve(__dirname, "./frontend/dist/"), //'/vehicle_app/frontend/dist/',
-    publicPath: 'http://localhost/vehicle_app/frontend/dist/',
-    // outputDir: '../'
+    publicPath: 'http://<enter project_url>/frontend/dist/',
+    // publicPath: 'http://localhost/vehicle_app/frontend/dist/',
     indexPath: '../../index.html',
-    // assetsPublicPath: '../../'
 };
