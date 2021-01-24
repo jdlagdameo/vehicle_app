76b7f8b82e4e7d0b85e923fe8ae02bd0cace0b46
John Dave Lagdameo <jdlagdameo@pldtglobal.com>
2021-01-24 12:34:45 +0800

    vuejs production build

---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+    // publicPath: path.resolve(__dirname, "./frontend/dist/"), //'/vehicle_app/frontend/dist/',
+    publicPath: 'http://localhost/vehicle_app/frontend/dist/',
+    // outputDir: '../'
+    indexPath: '../../index.html',
+    // assetsPublicPath: '../../'
+};
